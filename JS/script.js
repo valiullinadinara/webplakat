74ca12c97dd736055ca933a0fa606a0f0e6fa5e0
valiullinadinara <dianadura>
2019-03-26 00:32:00 +0300

    1
    1

---
+++ JS/script.js
@@ -0,0 +1,231 @@
+
+$('.p1').on("mouseenter",function() {
+  $('.p1').addClass('b');
+});
+
+$('.p2').on("mouseenter",function() {
+  $('.p2').addClass('b2');
+});
+
+$('.p3').on("mouseenter",function() {
+  $('.p3').addClass('b3');
+});
+
+$('.p4').on("mouseenter",function() {
+  $('.p4').addClass('b4');
+});
+
+$('.p5').on("mouseenter",function() {
+  $('.p5').addClass('b5');
+});
+
+$('.p6').on("mouseenter",function() {
+  $('.p6').addClass('b6');
+});
+
+$('.p7').on("mouseenter",function() {
+  $('.p7').addClass('b7');
+});
+
+$('.p8').on("mouseenter",function() {
+  $('.p8').addClass('b8');
+});
+
+$('.p9').on("mouseenter",function() {
+  $('.p9').addClass('b9');
+});
+
+$('.p10').on("mouseenter",function() {
+  $('.p10').addClass('b10');
+});
+
+$('.p11').on("mouseenter",function() {
+  $('.p11').addClass('b11');
+});
+
+$('.p12').on("mouseenter",function() {
+  $('.p12').addClass('b12');
+});
+
+$('.content').on("mouseenter",function() {
+  $('.content').addClass('b13');
+});
+
+$('.welcome').on("mouseenter",function() {
+  $('.welcome').addClass('b14');
+});
+
+
+
+
+$( "#img1" ).on("mouseenter", function() {
+  $( "#img1" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img3" ).animate({
+
+    right: "+=390",
+    bottom: "+=390"
+
+  }, 2000, function() {
+  });
+  $( "#img4" ).animate({
+
+    right: "+=590",
+    bottom: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img2" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+});
+
+$( "#img2" ).on("mouseenter", function() {
+  $( "#img1" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img3" ).animate({
+
+    right: "+=390",
+    bottom: "+=390"
+
+  }, 2000, function() {
+  });
+  $( "#img4" ).animate({
+
+    right: "+=590",
+    bottom: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img2" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+});
+
+$( "#img3" ).on("mouseenter", function() {
+  $( "#img1" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img3" ).animate({
+
+    right: "+=390",
+    bottom: "+=390"
+
+  }, 2000, function() {
+  });
+  $( "#img4" ).animate({
+
+    right: "+=590",
+    bottom: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img2" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+});
+
+$( "#img4" ).on("mouseenter", function() {
+  $( "#img1" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img3" ).animate({
+
+    right: "+=390",
+    bottom: "+=390"
+
+  }, 2000, function() {
+  });
+  $( "#img4" ).animate({
+
+    right: "+=590",
+    bottom: "+=590"
+
+  }, 2000, function() {
+  });
+  $( "#img2" ).animate({
+
+    left: "+=590",
+    top: "+=590"
+
+  }, 2000, function() {
+  });
+});
+
+// $('p1').on("mouseenter", function(){
+// 	this.addClass('go');
+// });
+
+
+
+// $( "#p6" ).on("mouseenter", function() {
+//   $( "#p6" ).animate({
+//
+//     left: "+=590",
+//     top: "+=-590"
+//
+//   }, 1000, function() {
+//   });
+//
+// });
+
+// var s = 18473902;
+// window.onload=function(){
+// setInterval(function(){
+//   s+=27838;
+//   document.getElementById("sum").children[0].innerHTML=s;
+// },1000)
+// }
+
+
+// $( "#p6" ).on("mouseenter", function() {
+//   $( "#p6" ).animate({
+//
+//     left: "+=590",
+//     top: "+=-590"
+//
+//   }, 1000, function() {
+//   });
+//
+// });
+
+// $( "#p7" ).on("mouseenter", function() {
+//   $( "#p7" ).animate({
+//    top:50;
+//    left: 70;
+//
+//
+//   }, 1000, function() {
+//   });
+//
+// });
